767bd34c1d2a57301f32509ba44863cfc5fa4279
JD Bothma <jbothma@gmail.com>
2020-08-13 21:52:52 +0200

    Add basic service worker maybe

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 document.getElementById('scratch').innerHTML = "It works";
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('serviceworker.js');
+  console.log("registering service worker");
+} else {
+  console.log("serviceWorker not supported");
+};
