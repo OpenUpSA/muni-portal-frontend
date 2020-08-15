621bcb96aba45602f2c595204ecf40a021da0756
JD Bothma <jbothma@gmail.com>
2020-08-15 18:07:57 +0200

    Add notifications

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,32 @@ if('serviceWorker' in navigator) {
 } else {
   console.log("serviceWorker not supported");
 };
+
+
+
+$("#btnSubscribeNotifications").on("click", () => {
+  Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+  });
+});
+
+$("#btnShowNotification").on("click", () => {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      const options = {
+        body: 'Here is a notification body!',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+        actions: [
+          {action: 'explore', title: 'Explore this new world',},
+          {action: 'close', title: 'Close notification',}
+        ]
+      };
+
+      reg.showNotification('Hello world!', options);
+    });
+  }
+});
--- src/serviceworker.js
+++ src/serviceworker.js
@@ -9,6 +9,7 @@ self.addEventListener('install', (e) => {
         '/index.html',
         '/manifest.webmanifest',
         'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+        'https://code.jquery.com/jquery-3.5.1.slim.min.js',
       ]);
     })
   );
@@ -29,4 +30,18 @@ addEventListener('fetch',  (event) => {
 
 self.addEventListener('activate', (e) => {
     console.log('[Service Worker] Activate');
+});
+
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = e.action;
+
+  if (action === 'close') {
+    notification.close();
+  } else {
+    console.log("User selected notification action {action}");
+    clients.openWindow('http://www.example.com');
+    notification.close();
+  }
 });
