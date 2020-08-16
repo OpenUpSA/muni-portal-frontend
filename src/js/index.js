d7ed7a0bf875308d51e70994c7088f5f6e6302d2
JD Bothma <jbothma@gmail.com>
2020-08-16 12:23:50 +0200

    Subscribe to the push service - doesn't workin on chrome:
    index.js:41 Unable to subscribe to push DOMException: Registration failed - missing applicationServerKey, and gcm_sender_id not found in manifest

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,21 @@
 document.getElementById('scratch').innerHTML = "It works";
 
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/serviceworker.js');
+  navigator.serviceWorker.register('/serviceworker.js').then((reg) => {
+    reg.pushManager.getSubscription().then(function(sub) {
+      if (sub === null) {
+        // Update UI to ask user to register for Push
+        console.log('Not subscribed to push service!');
+
+      } else {
+        // We have a subscription, update the database
+        console.log('Already sbscribed! Subscription object: ', sub);
+      }
+    }).catch(function(err) {
+      console.log('Service Worker registration failed: ', err);
+    });
+  });
+
   console.log("registering service worker");
 } else {
   console.log("serviceWorker not supported");
@@ -13,11 +27,27 @@ $("#btnSubscribeNotifications").on("click", () => {
   Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
   });
+
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function(reg) {
+      reg.pushManager.subscribe({
+        userVisibleOnly: true
+      }).then(function(sub) {
+        console.log("Subscribed to push!", sub);
+      }).catch(function(e) {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push', e);
+        }
+      });
+    });
+  }
 });
 
 $("#btnShowNotification").on("click", () => {
   if (Notification.permission == 'granted') {
-    navigator.serviceWorker.getRegistration().then(function(reg) {
+    navigator.serviceWorker.ready.then(function(reg) {
       const options = {
         body: 'Here is a notification body!',
         vibrate: [100, 50, 100],
@@ -26,8 +56,8 @@ $("#btnShowNotification").on("click", () => {
           primaryKey: 1
         },
         actions: [
-          {action: 'explore', title: 'Explore this new world',},
-          {action: 'close', title: 'Close notification',}
+          {action: 'explore', title: 'Explore',},
+          {action: 'close', title: 'Close',}
         ]
       };
 
