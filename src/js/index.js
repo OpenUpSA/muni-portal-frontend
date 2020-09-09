c705e57d74b76cb9d56a006559b1cc97273b36bb
JD Bothma <jbothma@gmail.com>
2020-09-10 00:40:34 +0200

    WIP skipping waiting

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,40 @@
-import logo36x36 from '../icon/lighthouse-icon-36x36.png';
-import exploreIcon from '../icon/explore-144x144.png';
-import logo620x620 from '../icon/lighthouse-icon.png';
-
+import {Workbox, messageSW} from 'workbox-window';
 
 const serviceWorkerPath = '/service-worker.js';
 
-if('serviceWorker' in navigator) {
+function makeSkipWaitingPrompt(wb, registration) {
+  return (event) => {
+    if (confirm("An update is available. Would you like to update the app now?")) {
+      wb.addEventListener('controlling', (event) => {
+        window.location.reload();
+      });
+
+      console.log(registration);
+      if (registration && registration.waiting) {
+        messageSW(registration.waiting, {type: 'SKIP_WAITING'});
+      } else {
+        console.log("How'd we get here?!");
+      }
+    } else {
+      console.log("Update rejected. Continue as is.");
+    }
+  };
+}
+
+
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     console.log("registering service worker");
-    navigator.serviceWorker.register(serviceWorkerPath);
+    const wb = new Workbox('/service-worker.js');
+    let registration;
+    const showSkipWaitingPrompt = makeSkipWaitingPrompt(wb, registration);
+
+    // Add an event listener to detect when the registered
+    // service worker has installed but is waiting to activate.
+    wb.addEventListener('waiting', showSkipWaitingPrompt);
+    wb.addEventListener('externalwaiting', showSkipWaitingPrompt);
+
+    wb.register().then((r) => registration = r);
   });
 } else {
   console.log("serviceWorker not supported");
