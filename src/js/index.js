30ec81fcaa6a97f4d24086ab89d605c5d2c18e98
JD Bothma <jbothma@gmail.com>
2020-09-10 00:49:27 +0200

    finally working installation prompt and skipwaiting installation

--- src/js/index.js
+++ src/js/index.js
@@ -2,40 +2,41 @@ import {Workbox, messageSW} from 'workbox-window';
 
 const serviceWorkerPath = '/service-worker.js';
 
-function makeSkipWaitingPrompt(wb, registration) {
-  return (event) => {
-    if (confirm("An update is available. Would you like to update the app now?")) {
-      wb.addEventListener('controlling', (event) => {
-        window.location.reload();
-      });
-
-      console.log(registration);
-      if (registration && registration.waiting) {
-        messageSW(registration.waiting, {type: 'SKIP_WAITING'});
-      } else {
-        console.log("How'd we get here?!");
-      }
-    } else {
-      console.log("Update rejected. Continue as is.");
-    }
-  };
-}
-
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     console.log("registering service worker");
     const wb = new Workbox('/service-worker.js');
     let registration;
-    const showSkipWaitingPrompt = makeSkipWaitingPrompt(wb, registration);
+    const showSkipWaitingPrompt =  (event) => {
+      if (confirm("An update is available. Would you like to update the app now?")) {
+        wb.addEventListener('controlling', (event) => {
+          window.location.reload();
+        });
+
+        console.log("skip waiting", registration);
+        if (registration && registration.waiting) {
+          messageSW(registration.waiting, {type: 'SKIP_WAITING'});
+        } else {
+          console.log("How'd we get here?!");
+        }
+      } else {
+        console.log("Update rejected. Continue as is.");
+      }
+    };
 
     // Add an event listener to detect when the registered
     // service worker has installed but is waiting to activate.
     wb.addEventListener('waiting', showSkipWaitingPrompt);
     wb.addEventListener('externalwaiting', showSkipWaitingPrompt);
 
-    wb.register().then((r) => registration = r);
+    wb.register().then((r) => {
+      console.log("then assign", r);
+      registration = r;
+    });
   });
 } else {
   console.log("serviceWorker not supported");
 };
+
+console.log("also js update");
