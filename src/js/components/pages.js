7afa6b34c175243c78035e28c5fe145e931358ce
JD Bothma <jbothma@gmail.com>
2020-10-07 13:21:35 +0200

    Add breadcrumb

--- src/js/components/pages.js
+++ src/js/components/pages.js
@@ -24,11 +24,13 @@ export class ModalPage {
 export class Service {
   constructor(service) {
     this.name = service.title;
+    this.breadcrumbItems = [{label: "Services", url: "#/services/"}];
   }
 
   render() {
     return [
       new PageTitle(this.name).render(),
+      new Breadcrumbs(this.breadcrumbItems).render(),
     ];
   }
 }
@@ -38,7 +40,39 @@ class PageTitle {
 
   constructor(title) {
     this.element = this.pageTitleTemplate.clone();
+    console.assert(this.element.length === 1);
     this.element.text(title);
+  }
+
+  render() {
+    return this.element;
+  }
+}
+
+class Breadcrumbs {
+  breadcrumbsTemplate = $(".styles .breadcrumbs__wrap").first();
+
+  constructor(items) {
+    this.element = this.breadcrumbsTemplate.clone();
+    this.itemsContainer = this.element.find(".breadcrumbs");
+    this.itemsContainer.empty();
+    items.forEach(item => {
+      this.itemsContainer.append(new Breadcrumb(item).render());
+    });
+  }
+
+  render() {
+    return this.element;
+  }
+}
+
+class Breadcrumb {
+  breadcrumbTemplate = $(".styles .breadcrumb").first();
+
+  constructor(item) {
+    this.element = this.breadcrumbTemplate.clone();
+    this.element.text(item.label);
+    this.element.attr("href", item.url);
   }
 
   render() {
