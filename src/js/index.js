95c1429c9b4614d909c8a77f5734dafe8d42fca1
JD Bothma <jbothma@gmail.com>
2020-10-07 11:08:48 +0200

    Just do our own router for now since the others suck

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import {tryRegisterSW} from './swRegistration.js';
 import {ServicesTab, TabContentContainer} from './components/tabs.js';
-import page from 'page';
 
 // Call as early as possible to maximise chance of registering reinstallation code
 tryRegisterSW();
@@ -13,31 +12,46 @@ class App {
   constructor() {
     const tabContentContainer = new TabContentContainer($(".tab-content"));
     this.servicesTab = new ServicesTab(settings, $(".main .tab-link").first(), tabContentContainer);
-
-    page('/', '/services');
-    page('/services', this.viewServices.bind(this));
-    page('/services/:service', this.viewService.bind(this));
-    page('*', function(){
-      $('body').text('Not found!')
-    });
-    page({
-      hashbang: true
-    });
-
-
+    this.router = new Router([
+      { path: new RegExp('^/services/$'), view: this.viewServices.bind(this) },
+      { path: /^\/services\/(?<service>[\w-]+)\/$/, view: this.viewService.bind(this) },
+      { path: new RegExp('.*'), view: function() { $('body').text('Not found!'); }},
+    ]);
   }
-
-
 
   viewServices() {
 
     this.servicesTab.show();
   }
 
-  viewService(context){
-    console.log("service", context.params.service);
+  viewService(params){
+    console.log("service", params.service);
   }
 
 }
 
+class Router {
+  constructor(routes) {
+    this.routes = routes;
+    window.addEventListener('hashchange', this.route.bind(this));
+    window.addEventListener('load', this.route.bind(this));
+  }
+
+  parseLocation() { return window.location.hash.slice(1).toLowerCase() || '/'; }
+
+  route() {
+    const location = this.parseLocation();
+    for (const route of this.routes) {
+      const match = route.path.exec(location);
+      console.log("trying", route);
+      if (match) {
+        console.debug(location, "matched", route);
+        route.view(match.groups);
+        return;
+      }
+    }
+
+    console.error("No match for ", location);
+  }
+}
 const app = new App();
