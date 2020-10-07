8460fcae9275f94b9c8e9b5b100ff09c4dd2390c
JD Bothma <jbothma@gmail.com>
2020-10-07 16:32:11 +0200

    Import webflow export'

--- src/js/webflow/import.js
+++ src/js/webflow/import.js
@@ -1,5 +1,6 @@
 exports.transform = function(window, $) {
   $("head").append('<meta name="theme-color" content="#0094ff">');
+  $("head").append('<link rel="manifest" href="manifest.webmanifest">');
 
   // Adding a script tag to body via jQuery seems to add it to head as well
   const tag = window.document.createElement("script");
