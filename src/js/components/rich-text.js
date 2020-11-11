f8d839a0b67cff321f2e91209c020b73bf4a4410
Schalk Neethling <sneethling@mozilla.com>
2020-11-11 10:01:01 +0200

    mark variables that are jQuery object with $

--- src/js/components/rich-text.js
+++ src/js/components/rich-text.js
@@ -1,35 +1,35 @@
 export class ExpandableRichText {
   constructor(html) {
-    this.template = $(".styles .expandable-rich-text");
-    this.element = this.template.clone();
-    this.contentContainer = this.element.find(".rich-text");
-    this.gradientContainer = this.element.find(".rich-text__gradient");
-    this.openButton = this.element.find(".expand-toggle__content-first");
-    this.closeButton = this.element.find(".expand-toggle__content-last");
+    this.$template = $(".styles .expandable-rich-text");
+    this.$element = this.$template.clone();
+    this.$contentContainer = this.$element.find(".rich-text");
+    this.$gradientContainer = this.$element.find(".rich-text__gradient");
+    this.$openButton = this.$element.find(".expand-toggle__content-first");
+    this.$closeButton = this.$element.find(".expand-toggle__content-last");
 
     // Interactions
-    this.openButton.on(
+    this.$openButton.on(
       "click",
       (() => {
-        this.contentContainer.addClass("expanded");
-        this.gradientContainer.addClass("expanded");
-        this.openButton.addClass("expanded");
+        this.$contentContainer.addClass("expanded");
+        this.$gradientContainer.addClass("expanded");
+        this.$openButton.addClass("expanded");
       }).bind(this)
     );
-    this.closeButton.on(
+    this.$closeButton.on(
       "click",
       (() => {
-        this.contentContainer.removeClass("expanded");
-        this.gradientContainer.removeClass("expanded");
-        this.openButton.removeClass("expanded");
+        this.$contentContainer.removeClass("expanded");
+        this.$gradientContainer.removeClass("expanded");
+        this.$openButton.removeClass("expanded");
       }).bind(this)
     );
 
     // Content
-    this.contentContainer.html(html);
+    this.$contentContainer.html(html);
   }
 
   render() {
-    return this.element;
+    return this.$element;
   }
 }
