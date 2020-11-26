9a5d37fcf68d51fabed7852680f940b8a30d1bb5
Schalk Neethling <sneethling@mozilla.com>
2020-11-26 13:55:35 +0200

    feat: in app notifications
    Enable user settings page and ability to enable push notifications
    
    fix #64

--- src/js/components/account/user-settings.js
+++ src/js/components/account/user-settings.js
@@ -8,18 +8,19 @@ export class UserSettings {
   constructor() {
     const $container = $("<div />");
     const accountSettingsSections = [];
-    const $emailNotifications = getCustomCheckbox({
-      identifier: "notify-email",
-      name: "notify-email",
-      text: "Receive email notifications",
-    });
-    const $inAppNotifications = getCustomCheckbox({
+    const $inAppNotificationsContainer = getCustomCheckbox({
       identifier: "notify-inapp",
       name: "notify-inapp",
       text: "Receive in-app notifications",
     });
+    this.$inAppNotificationsCutomCheckbox = $inAppNotificationsContainer.find(
+      ".w-checkbox-input"
+    );
+    this.$inAppNotificationsCheckbox = $inAppNotificationsContainer.find(
+      "input[type='checkbox']"
+    );
     const $inAppNotificationsNoSupportMsg = new BasicBlock({
-      title: "In-app notifications is not supported on your device 😞",
+      title: "In-app notifications is not supported on your device",
       subtitle: "",
     });
 
@@ -42,26 +43,50 @@ export class UserSettings {
 
     $container.append(new SectionHeading("Notification settings").render());
 
+    // is Notifications supported?
     if (!("Notification" in window)) {
       $container.append($inAppNotificationsNoSupportMsg.render());
     } else {
-      $container.append($inAppNotifications);
-      $inAppNotifications.click(() => {
-        this.setInAppNotificationState();
-      });
-    }
+      // check the current Notification.permission status
+      // and update the state of the checkbox if permission
+      // was already granted
+      if (Notification.permission === "granted") {
+        this.$inAppNotificationsCutomCheckbox.addClass("w--redirected-checked");
+        this.$inAppNotificationsCheckbox.checked = true;
+      }
 
-    $container.append($emailNotifications);
+      $container.append($inAppNotificationsContainer);
+
+      // if the user has neither denied not granted permissions before
+      if (
+        Notification.permission !== "granted" &&
+        Notification.permission !== "denied"
+      ) {
+        $inAppNotificationsContainer.click(() => {
+          // ask for permission to send notifications
+          this.setInAppNotificationState();
+        });
+      }
+    }
 
     this.$element = $container;
   }
 
   setInAppNotificationState() {
-    Notification.requestPermission().then((response) => {
-      if (response !== "granted") {
-        alert("FUCK YOU!!!!!!!!!!");
-      }
-    });
+    Notification.requestPermission()
+      .then((response) => {
+        if (response === "granted") {
+          this.$inAppNotificationsCutomCheckbox.addClass(
+            "w--redirected-checked"
+          );
+          this.$inAppNotificationsCheckbox.checked = true;
+        }
+      })
+      .catch((error) => {
+        console.error(
+          `Error while requesting notification permission: ${error.toString()}`
+        );
+      });
   }
 
   render() {
