01b10d7d56149232cefe65a4661d9b66e105fca8
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-01-25 23:01:37 +0200

    Add login link if password reset was successful

--- src/js/components/account/reset-password.js
+++ src/js/components/account/reset-password.js
@@ -1,5 +1,6 @@
 import { API } from "../../api";
 import {
+  getAnchorElement,
   getDiv,
   getForm,
   getInput,
@@ -11,6 +12,8 @@ export class ResetPassword {
   constructor() {
     const $successTemplate = $(".styles .form-styles .w-form-done").clone();
     const $failTemplate = $(".styles .form-styles .w-form-fail").clone();
+    const $loginAnchor = getAnchorElement('/accounts/login/', 'link-text form-submit', 'Please log in with your new password');
+    $loginAnchor.hide()
 
     const defaultBaseUrl = "https://muni-portal-backend.openup.org.za";
     const endPoint = "/api/accounts/reset-password/";
@@ -35,10 +38,11 @@ export class ResetPassword {
     $resetFormContainer.append($form);
     $resetFormContainer.append($successTemplate);
     $resetFormContainer.append($failTemplate);
+    $resetFormContainer.append($loginAnchor);
 
     $form.submit((event) => {
       event.preventDefault();
-      this.resetPassword(endPoint, $form, $successTemplate, $failTemplate);
+      this.resetPassword(endPoint, $form, $successTemplate, $failTemplate, $loginAnchor);
     });
 
     this.$element = $resetFormContainer;
@@ -50,8 +54,9 @@ export class ResetPassword {
    * @param {jqObject} $form - the form to submit
    * @param {jqObject} $success - reference to the success template
    * @param {jqObject} $fail  - reference to the failure template
+   * @param {jqObject} $loginAnchor - reference to the login anchor
    */
-  resetPassword(endPoint, $form, $success, $fail) {
+  resetPassword(endPoint, $form, $success, $fail, $loginAnchor) {
     const api = new API();
     const response = api.resetPassword(endPoint, $form.serialize());
     response
@@ -61,9 +66,10 @@ export class ResetPassword {
           $success
             .empty()
             .append(
-              "Your password was changed successfully."
+              `Your password was changed successfully.`
             )
             .show();
+          $loginAnchor.show();
         }
       })
       .fail((jqXHR, textStatus) => {
