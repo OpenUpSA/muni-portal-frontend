2fd23c26440f45c55de7cd049077a52d2a389fb1
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-01-27 21:16:09 +0200

    Redirect to login page if refresh failed and refactoring to improve readability, understandability and DRY

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,9 @@
 const defaultBaseUrl = "https://muni-portal-backend.openup.org.za";
 
+function getBaseApiUrl() {
+  return sessionStorage.getItem("apiBaseUrl") || defaultBaseUrl;
+}
+
 export class API {
   constructor() {
     if (window.location.search.includes("promptapi"))
@@ -7,7 +11,7 @@ export class API {
         "apiBaseUrl",
         window.prompt("Enter the API base URL", defaultBaseUrl)
       );
-    this.baseUrl = sessionStorage.getItem("apiBaseUrl") || defaultBaseUrl;
+    this.baseUrl = getBaseApiUrl()
   }
 
   getAdministrationIndex() {
@@ -177,29 +181,60 @@ export class API {
   }
 }
 
+/*
+  Any AJAX request that fails due to a 401 status code (unauthorised) will
+  refresh the access token by doing a POST with the local refresh token
+  and then repeat the original request with the new access token.
+
+  In other words, if the access token expires, we fetch a new one and continue
+  as normal.
+*/
+$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+  // enable this on a retry request to avoid infinite recursion
+  if (options.refreshRequest) {
+    return;
+  }
+
+  let deferred = $.Deferred();
+
+  jqXHR.done(deferred.resolve);
+
+  jqXHR.fail(() => {
+    const args = Array.prototype.slice.call(arguments);
+    if (jqXHR.status === 401) {
+      retryAjaxWithRefreshedToken(deferred, jqXHR, args, originalOptions);
+    } else {
+      deferred.rejectWith(jqXHR, args);
+    }
+  });
+
+  // Now override the jqXHR's promise functions with our deferred
+  return deferred.promise(jqXHR);
+});
+
 function handleRefreshTokenExpired(deferred, jqXHR, args) {
-  alert("Your session has expired. Sorry.");
+  localStorage.removeItem("user");
+  localStorage.removeItem("refresh");
   // reject with the original 401 response
   deferred.rejectWith(jqXHR, args);
+  window.location.replace("/accounts/login/");
 }
 
 function handleAccessTokenRefreshed(response, deferred, originalOptions) {
-  window.console.log("refresh was successful, retrying original request");
-  // set new access token
   localStorage.setItem("user", response.access);
-  // retry with a copied originalOpts with refreshRequest.
-  let newOpts = $.extend({}, originalOptions, {
+  // retry original request with refreshRequest and the new access token
+  let newOptions = $.extend({}, originalOptions, {
     refreshRequest: true,
     headers: {
       authorization: `Bearer ${response.access}`,
     },
   });
   // pass this one on to our deferred pass or fail.
-  $.ajax(newOpts).then(deferred.resolve, deferred.reject);
+  $.ajax(newOptions).then(deferred.resolve, deferred.reject);
 }
 
 function retryAjaxWithRefreshedToken(deferred, jqXHR, args, originalOptions) {
-  const baseUrl = sessionStorage.getItem("apiBaseUrl") || defaultBaseUrl;
+  const baseUrl = getBaseApiUrl();
   const url = `${baseUrl}/api/token/refresh/`;
   const refreshToken = `${localStorage.getItem("refresh")}`;
 
@@ -217,42 +252,3 @@ function retryAjaxWithRefreshedToken(deferred, jqXHR, args, originalOptions) {
     },
   });
 }
-
-/*
-  Any AJAX request that fails due to a 401 status code (unauthorised) will
-  refresh the access token by doing a POST with the local refresh token
-  and then repeat the original request with the new access token.
-
-  In other words, if the access token expires, we fetch a new one and continue
-  as normal.
-*/
-$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-  // enable this on a retry request to avoid infinite recursion
-  window.console.log("running prefilter");
-  if (options.refreshRequest) {
-    window.console.log("refreshRequest === true, returning");
-    return;
-  }
-
-  // our own deferred object to handle done/fail callbacks
-  let deferred = $.Deferred();
-
-  // if the request is successful, return normally
-  jqXHR.done(deferred.resolve);
-
-  // if the request fails, do something else but still resolve
-  jqXHR.fail(() => {
-    const args = Array.prototype.slice.call(arguments);
-    window.console.log("request failed");
-    if (jqXHR.status === 401) {
-      window.console.log("status was 401, attempting refresh");
-      retryAjaxWithRefreshedToken(deferred, jqXHR, args, originalOptions);
-    } else {
-      window.console.log("status was not 401");
-      deferred.rejectWith(jqXHR, args);
-    }
-  });
-
-  // Now override the jqXHR's promise functions with our deferred
-  return deferred.promise(jqXHR);
-});
