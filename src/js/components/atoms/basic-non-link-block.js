f2b608ffbc84bd672c5912eb6531b3ded2670d13
Schalk Neethling <sneethling@mozilla.com>
2021-03-18 17:12:30 +0200

    update basic non link block component

--- src/js/components/atoms/basic-non-link-block.js
+++ src/js/components/atoms/basic-non-link-block.js
@@ -1,7 +1,7 @@
 export default class BasicNonLinkBlock {
   constructor({ content }) {
-    this.$element = $("<div />", { class: "card basic-block" });
-    this.$element.append(content);
+    this.$element = $(".components .card.basic-block:eq(0)").clone();
+    this.$element.find(".link-block__inner").empty().append(content);
   }
 
   render() {
