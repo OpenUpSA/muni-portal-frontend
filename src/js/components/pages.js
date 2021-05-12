a9173ea1c1d520749edcbf693d911934fb47e7ae
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-05-12 15:52:30 +0200

    Add basic NewsIndexPage and NewsPage classes

--- src/js/components/pages.js
+++ src/js/components/pages.js
@@ -409,6 +409,10 @@ export class NoticePage {
   }
 }
 
+export class NewsIndexPage extends NoticeIndexPage {}
+
+export class NewsPage extends NoticePage {}
+
 export class ContactsPage {
   constructor(content) {
     this.name = content.title;
