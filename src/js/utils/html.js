7a1e94d545456c64c6a837a325fae98bdae75ede
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-05-17 14:16:04 +0200

    Only prepend API url to image paths if http or https is not present in the string

--- src/js/utils/html.js
+++ src/js/utils/html.js
@@ -1,18 +1,23 @@
 import { getBaseApiUrl } from "../api";
 /*
   Takes an HTML string and replaces each 'src' attribute in each 'img' tag with
-  the API url prepended.
+  the API url prepended, if it does not already have 'http' or 'https' prepended
+
+  This is not necessary for URLs hosted on S3 but it is for django's local
+  filesystem storage.
 
   E.g. /path/to/img.jpg -> http://api.com/path/to/img.jpg
  */
 export function parseImgTags(html) {
-  let parsedHTML = document.createElement("div");
+  const parsedHTML = document.createElement("div");
   parsedHTML.innerHTML = html;
-  let imgTags = parsedHTML.querySelectorAll("img");
+  const imgTags = parsedHTML.querySelectorAll("img");
 
   imgTags.forEach(function (img) {
-    let src = img.getAttribute("src");
-    img.setAttribute("src", getBaseApiUrl() + src);
+    const src = img.getAttribute("src");
+    if (!src.includes("http://") || !src.includes("https://")) {
+      img.setAttribute("src", getBaseApiUrl() + src);
+    }
   });
 
   return parsedHTML;
