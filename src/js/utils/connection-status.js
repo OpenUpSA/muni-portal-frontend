793d77e51c729606a2282e5169b7d5ee347bbef6
Schalk Neethling <sneethling@mozilla.com>
2021-08-03 20:57:40 +0200

    fix: improved offline experience
    Improves the offline experience and also shows a message to the user when offline.

---
+++ src/js/utils/connection-status.js
@@ -0,0 +1,39 @@
+function getOfflineBanner() {
+  const bannerStyle = {
+    backgroundColor: "rgba(255,255,255,0.5)",
+    borderRadius: "6px",
+    display: "none",
+    fontWeight: "bold",
+    margin: "0 auto 24px",
+    maxWidth: "80%",
+    padding: "12px 24px",
+  };
+  return $("<div />", {
+    text: "You are currently offline. Some app functionality might not work.",
+    class: "offline-status-banner",
+  }).css(bannerStyle);
+}
+
+export function showConnectionStatus() {
+  let $offlineStatusBanner = null;
+  let isOnline = "onLine" in navigator ? navigator.onLine : true;
+  $(".nav").after(getOfflineBanner());
+
+  if (!isOnline) {
+    $offlineStatusBanner = $(".offline-status-banner");
+    $offlineStatusBanner.show();
+    isOnline = false;
+  }
+
+  window.addEventListener("online", () => {
+    $offlineStatusBanner = $offlineStatusBanner || $(".offline-status-banner");
+    $offlineStatusBanner.hide();
+    isOnline = true;
+  });
+
+  window.addEventListener("offline", () => {
+    $offlineStatusBanner = $offlineStatusBanner || $(".offline-status-banner");
+    $offlineStatusBanner.show();
+    isOnline = false;
+  });
+}
--- src/service-worker.js
+++ src/service-worker.js
@@ -6,6 +6,10 @@ importScripts("https://pushpad.xyz/service-worker.js");
 // https://developers.google.com/web/tools/workbox/modules/workbox-cli#injectmanifest
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
+const handler = workbox.precaching.createHandlerBoundToURL("/index.html");
+const navigationRoute = new workbox.routing.NavigationRoute(handler);
+workbox.routing.registerRoute(navigationRoute);
+
 // workbox
 workbox.routing.registerRoute(
   ({ url }) => url.origin === "https://storage.googleapis.com",
@@ -21,11 +25,14 @@ workbox.routing.registerRoute(
 // external domains using a cache first strategy
 workbox.routing.registerRoute(
   ({ url }) =>
+    url.origin === "https://ajax.googleapis.com" ||
+    url.origin === "https://ka-f.fontawesome.com" ||
     url.origin === "https://kit.fontawesome.com" ||
     url.origin === "https://d3e54v103j8qbb.cloudfront.net" ||
+    url.origin === "https://unpkg.com" ||
     url.origin === "https://fonts.googleapis.com" ||
     url.origin === "https://fonts.gstatic.com",
-  new workbox.strategies.CacheFirst()
+  new workbox.strategies.NetworkFirst()
 );
 
 addEventListener("message", (event) => {
