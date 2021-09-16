796c4531e67b588db90fa851d4764501c7fd1025
JD Bothma <jbothma@gmail.com>
2021-09-16 23:03:20 +0200

    Fix naming and use fixture as a cypress fixture

--- cypress/integration/listServices_spec.js
+++ cypress/integration/listServices_spec.js
@@ -1,22 +1,22 @@
-import getServicesResponse from './sample-get-services.js';
-
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
+describe('List services', () => {
+  it('Links to the services', () => {
+    // Mock all requests to the backend as obviously broken
     cy.intercept({
       method: 'GET',
       url: 'https://muni-portal-backend.openup.org.za/api/**',
     }, {statusCode: 500}).as('unMocked');
 
+    // Mock the request relevant to this test to return services for test
     cy.intercept({
       method: 'GET',
       url: '**/api/wagtail/v2/pages/?type=core.ServicePage&fields=*&limit=100',
     }, {
       statusCode: 200,
-      body: getServicesResponse,
+      fixture: 'listServices-getServices.json',
       headers: {'access-control-allow-origin': '*'} ,
     }).as('getServices');
 
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:8080');
 
     cy.wait('@getServices');
 
