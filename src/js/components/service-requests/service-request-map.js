774c57a758b5936ac3a2c45a39512c1dce9b1712
desafinadude <meerkdir@gmail.com>
2022-02-23 12:22:37 +0200

    Moved dropdown z-index fix to CSS

--- src/js/components/service-requests/service-request-map.js
+++ src/js/components/service-requests/service-request-map.js
@@ -58,10 +58,6 @@ function getLocation(map) {
  */
 export const initMap = () => {
 
-
-  var elem = document.querySelector('.dropdown.w-dropdown');
-  elem.style.zIndex = '10000';
-
   // ensure leafletjs loaded successfully before using it
   if (L) {
     const defaultCoordinates = [-34.8311103, 20.0043008];
